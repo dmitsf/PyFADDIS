d60eee52ba7a8e15a7c9c5727fe45ea5ea5a744f
Dmitry Frolov <dmitsf@gmail.com>
2020-02-13 14:07:27 +0300

    lapin transform

--- lapin.py
+++ lapin.py
@@ -8,39 +8,27 @@ ENTITY_BOUND = 10 ** (-4)
 def lapin(A):
     A = (A + A.T) / 2
     a_sums = np.ravel(abs(sum(A)))
-    t = np.array(a_sums > ENTITY_BOUND)
-    correct = t.all()
+    checked = np.array(a_sums > ENTITY_BOUND)
+    is_correct = checked.all()
 
-    if not correct:
+    if not is_correct:
         print('These entities are no good - remove them first!!!')
-        print([i for i, j in enumerate(t, 1) if not j])
+        print([i for i, j in enumerate(checked, 1) if not j])
+        A = A[:, checked][checked, :]
+        a_sums = a_sums[checked]
 
-    nn, nn = A.shape
-    if correct:
-        AR = A
-        ar = a_sums
-    else:
-        AR = A[:, t][t, :]
-        ar = a_sums[t]
-
-    n, n = AR.shape
-    D = np.eye(n)
+    n, _ = A.shape
     C = np.empty([n, n])
+    for i in range(n):
+        for j in range(n):
+            C[i, j] = A[i, j] / np.sqrt(a_sums[i] * a_sums[j])
 
-    ar = np.ravel(ar)
-    for ii in range(n):
-        for jj in range(n):
-            C[ii, jj]=AR[ii, jj] / np.sqrt(ar[ii] * ar[jj])
-
-    Bt = D - C
-    Ln = Bt
-    ll, vv = LA.eig(Bt)
-    ld = np.diag(ll)
-    cond = np.array(ll > ZERO_BOUND)
-
-    lcd = ld[cond, :][:, cond]
-    vd = vv[:, cond]
-    B = vd.dot(LA.inv(lcd)).dot(vd.T)
+    eig_vals, eig_vecs = LA.eig(np.eye(n) - C)
+    eig_vals_diag = np.diag(eig_vals)
+    nonzero_cond = np.array(eig_vals > ZERO_BOUND)
+    nonzero_eig_vals_diag = eig_vals_diag[nonzero_cond, :][:, nonzero_cond]
+    nonzero_eig_vecs = eig_vecs[:, nonzero_cond]
+    B = nonzero_eig_vecs.dot(LA.inv(nonzero_eig_vals_diag)).dot(nonzero_eig_vecs.T)
 
     return B
 
@@ -48,6 +36,6 @@ def lapin(A):
 if __name__ == '__main__':
 
     M = np.matrix([[1, 2, 1], [2, 4, 1], [1, 1, 9]])
-    M = np.matrix([[1, 0, 1], [0, 0, 0], [1, 0, 9]])
+    M = np.matrix([[1, 0, 1], [0, 3, 0], [1, 0, 9]])
     M_transformed = lapin(M)
     print(M_transformed)
